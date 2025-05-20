5f9d5de732cd2325ee9b1c9efe9efd362839a3b3
Lunix-420 <davidhess336@gmx.de>
2025-05-20 02:56:51 +0200

    feat: Implement credits menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 @onready var main_menu: Node2D = get_node("MainMenu")
 @onready var help_menu: Node2D = get_node("HelpMenu")
+@onready var credits_menu:  Node2D = get_node("CreditsMenu")
 @onready var player: CharacterBody2D = get_parent().get_node("Player")
 
 func _on_start_button_pressed() -> void:
@@ -15,7 +16,21 @@ func _on_exit_button_pressed() -> void:
 func _on_controls_button_pressed() -> void:
 	main_menu.visible = false
 	help_menu.visible = true
+	credits_menu.visible = false
 	
 func _on_exit_help_button_pressed() -> void:
 	main_menu.visible = true
 	help_menu.visible = false
+	credits_menu.visible = false
+
+
+func _on_exit_credits_button_pressed() -> void:
+	main_menu.visible = true
+	help_menu.visible = false
+	credits_menu.visible = false
+
+
+func _on_credits_button_pressed() -> void:
+	main_menu.visible = false
+	help_menu.visible = false
+	credits_menu.visible = true
