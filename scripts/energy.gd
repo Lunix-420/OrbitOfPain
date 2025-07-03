380393e878560b629df7945ebeb8aea09416152f
Lunix-420 <davidhess336@gmx.de>
2025-07-03 09:35:40 +0200

    feat: Remove energy after timer

--- scripts/energy.gd
+++ scripts/energy.gd
@@ -5,17 +5,31 @@ extends Area2D
 
 @onready var world: Node2D = get_parent()
 @onready var player: Node2D = world.get_node("Player")
+@onready var emitter: GPUParticles2D = get_node("GPUParticles2D")
 
 #==================================================================================================#
 # Parameters
 
 const MAX_MAGNET_DISTANCE := 2000.0
 const MAX_MAGNET_SPEED := 500.0
+const LIFETIME := 15.0
+
+#==================================================================================================#
+# Timers
+
+var lifetime_timer := 0.0
 
 #==================================================================================================#
 # Main Behaviors
 
 func _physics_process(delta: float) -> void:
+	lifetime_timer += delta
+	if lifetime_timer >= LIFETIME * 0.9:
+		emitter.emitting = false
+	if lifetime_timer >= LIFETIME:
+		queue_free()
+	print(lifetime_timer)
+	
 	if not is_instance_valid(player):
 		return
 	
